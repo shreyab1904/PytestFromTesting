c5f45620d3a8d78b3837f0234b3f7ea49784f50c
shreyab1904 <shreyaborkar0201@gmail.com>
2022-03-31 17:05:24 +0530

    done

---
+++ test_form.py
@@ -0,0 +1,28 @@
+import time
+
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+@pytest.fixture()
+def setUp():
+    global name,driver
+    name=input("enter your name: ")
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield
+    time.sleep(5)
+    driver.close()
+
+def test_form(setUp):
+    driver.get("https://iprimedtraining.herokuapp.com/")
+    time.sleep(1)
+    driver.find_element_by_name("name").send_keys(name)
+    time.sleep(1)
+    driver.find_element_by_xpath("/html/body/div/div/div[2]/form/table/tbody/tr[2]/td[2]/input[2]").click()
+    time.sleep(1)
+    driver.find_element_by_xpath("/html/body/div/div/div[2]/form/table/tbody/tr[3]/td[2]/select/option[2]").click()
+    time.sleep(1)
+    driver.find_element_by_name("fcheckbox").click()
+    time.sleep(1)
+    driver.find_element_by_name("subbtn").click()
+    time.sleep(9)
